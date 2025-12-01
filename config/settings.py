7bf4abf6e2bcf4d2578b66eeb91bdf45e8c095bf
Bruno Leite <brunomatheusstefanoleite@gmail.com>
2025-12-01 18:29:27 -0300

    Conversão de arquivos
    - Transformar arquivos de doc para pdf
    - Criar uma pasta
    - Mover arquivos pdf para a pasta

---
+++ config/settings.py
@@ -0,0 +1,6 @@
+# configurações / constantes do projeto
+# por exemplo: destino SharePoint, caminhos base etc
+
+# settings.py
+pasta_input = "C:\\Users\\55119\\Documents\\Projetos_e_Estudos\\Automacao\\ProjetoBMSL-Organizador_Arquivos\\input_files"
+pasta_output = "C:\\Users\\55119\\Documents\\Projetos_e_Estudos\\Automacao\\ProjetoBMSL-Organizador_Arquivos\\output_files"
