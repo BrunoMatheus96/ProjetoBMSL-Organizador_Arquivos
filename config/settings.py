7f072a33557f5201e3643e3232dd8e91c303f728
Bruno Leite <brunomatheusstefanoleite@gmail.com>
2025-12-02 19:16:41 -0300

    Ajustes

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,6 @@
 # configurações / constantes do projeto
 # por exemplo: destino SharePoint, caminhos base etc
 
-# settings.py
 pasta_input = "C:\\Users\\55119\\Documents\\Projetos_e_Estudos\\Automacao\\ProjetoBMSL-Organizador_Arquivos\\input_files"
 xlsx_input = "C:\\Users\\55119\\Documents\\Projetos_e_Estudos\\Automacao\\ProjetoBMSL-Organizador_Arquivos\\input_files\\Excel_para_teste.xlsx"
 pasta_output = "C:\\Users\\55119\\Documents\\Projetos_e_Estudos\\Automacao\\ProjetoBMSL-Organizador_Arquivos\\output_files"
--- src/upload.py
+++
@@ -1,7 +0,0 @@
-# Lógica de upload para SharePoint
-
-def upload_cloud():
-    # Criar uma pasta dentro de outra pasta no Sharepoint
-
-    # Subir pasta zipada + excel na pasta criada no Sharepoint
-    pass
